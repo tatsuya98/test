8c25bcf1f4c32cebccc846380180c12e518ee6dc
tatsuya98 <nathanbailey_62@hotmail.com>
2021-07-11 00:30:23 +0100

    Updates

---
+++ precache-manifest.72746d6f3270e9875769f700e569bdc3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b6d27996d651935367807a89ec4303",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "a76a39f328a407111839",
+    "url": "/test/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "c5c677fa4b2017b0670a",
+    "url": "/test/static/css/main.5c28e3aa.chunk.css"
+  },
+  {
+    "revision": "a76a39f328a407111839",
+    "url": "/test/static/js/2.c7b8b75f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/test/static/js/2.c7b8b75f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5c677fa4b2017b0670a",
+    "url": "/test/static/js/main.21e0f50a.chunk.js"
+  },
+  {
+    "revision": "a92632ed06e82fb88a6c",
+    "url": "/test/static/js/runtime-main.4571773e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/test/static/media/SEGA.12a3db8b.woff"
+  }
+]);
